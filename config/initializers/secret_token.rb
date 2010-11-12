92b4bd511c27714ff4598ae0d99865adf7aef640
evanburchard <evan.burchard@gmail.com>
2010-11-11 22:14:28 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = '9610b46ae70528a9484c68c6a24b8f4ab84ed1e903f817ec883f0cd0104fded1e6fa6c3bf0cc05932a3814c35362cba4fa82e23c7bf93f0de6cc3a82843ac6de'
